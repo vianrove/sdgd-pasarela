aaaacf0d6d8daf3224b3fe1111abd6dc51942437
vianrove <thuanromero@gmail.com>
2022-10-29 17:48:00 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.use(express.json());
+
+app.listen(
+    PORT,
+    () => console.log(`API live on http://localhost:${PORT}`)
+);
+
+app.get('/', (req, res) => res.send('Pasarela de pagos homepage'));
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to Mongo'))
+    .catch((error) => console.error(error));
